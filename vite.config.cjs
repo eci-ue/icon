1d93ae085524690315288f93c7877a20969b1973
svonme <svon.me@gmail.com>
2023-06-25 17:34:20 +0800

    feat: 测试

---
+++ vite.config.cjs
@@ -0,0 +1,41 @@
+const path = require("path");
+const { defineConfig } = require("vite");
+const vue = require("@vitejs/plugin-vue");
+const jsx = require("@vitejs/plugin-vue-jsx").default;
+
+export default defineConfig({
+  resolve: {
+    extensions: [".ts", ".vue", ".js", ".tsx"],
+    alias: {
+      "src/": `${path.resolve(__dirname, "src")}/`,
+      "test/": `${path.resolve(__dirname, "test")}/`,
+      "types/": `${path.resolve(__dirname, "types")}/`,
+    },
+  },
+  plugins: [vue(), jsx()],
+  build: {
+    target: "modules",
+    polyfillModulePreload: false,
+    lib: {
+      entry: "src/index",
+      name: "icon",
+      formats: ["es"],
+      fileName: "icon"
+    },
+    sourcemap: true,
+    manifest: false,
+    rollupOptions: {
+      external: [
+        /^lodash/i,
+        /^vue/i,
+        /^@ue/i,
+        /@ant-design/i,
+        /@fengqiaogang/i,
+        /ant-design-vue/i,
+      ],
+      output: {
+        inlineDynamicImports: true
+      }
+    }
+  }
+})
